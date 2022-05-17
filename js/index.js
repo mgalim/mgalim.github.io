c01eb854776f0b28866a0bca012657d88dcff48a
Marcelo Galimberti <galimbertimarcelo@gmail.com>
2022-05-17 20:24:20 -0300

    se agrego form get y cambio codigo js

--- js/index.js
+++ js/index.js
@@ -8,11 +8,17 @@ calcular = function () {
   document.getElementById("total").innerHTML = total.toFixed(2);
 };
 
-reset = function () {
-  var element = document.getElementsByClassName("form-control");
-  for (let i = 0; i < element.length; i++) {
-    element[i].value = "";
-  }
+limpiar = function () {
   document.getElementById("total").innerHTML = "";
-  document.getElementById("categoria").value = "80";
 };
+
+// Funcion limpiar sin formulario
+
+// reset = function () {
+//   var element = document.getElementsByClassName("form-control");
+//   for (let i = 0; i < element.length; i++) {
+//     element[i].value = "";
+//   }
+//   document.getElementById("total").innerHTML = "";
+//   document.getElementById("categoria").value = "80";
+// };
